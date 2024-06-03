62e78b54f7bd2dfa87d809d240d0616c6661fcd2
ManoGirgis <119486867+ManoGirgis@users.noreply.github.com>
2024-06-03 16:32:43 +0200

    error.js

--- src/components/Common/Error.js
+++ src/components/Common/Error.js
@@ -1,4 +1,5 @@
 export default function Error() {
+    console.log(`${process.env.REACT_APP_HOST_URL}`)
     return (
         <>
             <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
@@ -8,7 +9,7 @@ export default function Error() {
                     <p className="mt-6 text-base leading-7 text-gray-600">Sorry, we couldn’t find the page you’re looking for.</p>
                     <div className="mt-10 flex items-center justify-center gap-x-6">
                         <a
-                            href="./../"
+                            href={process.env.REACT_APP_HOST_URL}
                             className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                         >
                             Go back home
